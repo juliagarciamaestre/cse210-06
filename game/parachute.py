4544793d20c6fbe7e8c7aa7a30d9e11079ded99a
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-02-08 09:38:46 -0300

    Added text for drawing the parachutte.

--- game/parachute.py
+++ game/parachute.py
@@ -1,3 +1,32 @@
 # !!! IMPORTS
 
 class Parachute:
+  def __init__(self):
+        self._parachutte=[]
+    
+   def build_parachute(self):
+
+
+        self._parachutte=[      ['_', '_', '_', '_', '_'],
+                                [' ', ' ', ' ', ' ', ' '],
+                                [' ', '_', ' ', '_', ' '],
+                                ['/', '-', ' ', '-', '\\'],
+                                ['\\', ' ', ' ', ' ', '/'],
+                                [' ', '\\', ' ', '/', ' '],
+                                [' ', ' ', 'O', ' ', ' '],
+                                [' ', '/', '|', '\\', ' ', ' '],
+                                [' ', '/', ' ', '\\', ' '],
+                         ]
+        
+        print("\t")
+        #print the parachutte 
+        # matrix : 7rows x 5 columns                          
+        for i in range(0,9):
+            for j in range(0,5):
+                self._spl = self._parachutte[i][j]
+                print(self._spl, end=" ")
+            print("\n\t")
+        #Ground    
+        print("^^^^^^^^^^^^^^^^^")    
+
+
