5375545067fa5b71bcf9d1315a2f9b2ad0303a65
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-02-08 17:27:53 -0300

    Added code to SecretWord class, wad added __init__() , _word_list(), _word()  methods

--- game/secret_word.py
+++ game/secret_word.py
@@ -1,3 +1,21 @@
 # !!! IMPORTS
+import random
 
 class SecretWord:
+  def __init__(self):
+        #Container for the word to guess
+        self._wordd=""
+        #Counter for chars found in self._wordd
+        self._contador=0
+        #container for the char index found in self._word
+        self._indice=[]
+        
+
+    def _word_list(self):
+        self._word_list=["david","ernesto","benjamin","samuel","gerardo"]
+        random.shuffle(self._word_list)
+        
+    def _word(self):
+        z = random.randint(0,4)
+        self._wordd = self._word_list[z]
+        return self._wordd
