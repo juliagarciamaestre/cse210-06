0673352e592241dff0bfead0b409cb1979bf6f26
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-02-10 11:21:32 -0300

    Added logic code to secret_word.py

--- game/secret_word.py
+++ game/secret_word.py
@@ -2,20 +2,30 @@
 import random
 
 class SecretWord:
-  def __init__(self):
-        #Container for the word to guess
+    def __init__(self):
         self._wordd=""
-        #Counter for chars found in self._wordd
+        #Counter for char found in self._word
         self._contador=0
         #container for the char index found in self._word
         self._indice=[]
         
-
+    #Mezcla la lista
     def _word_list(self):
         self._word_list=["david","ernesto","benjamin","samuel","gerardo"]
         random.shuffle(self._word_list)
-        
+    #Retorna una palabra de la lista    
     def _word(self):
         z = random.randint(0,4)
         self._wordd = self._word_list[z]
         return self._wordd
+        
+
+    def _deleteLine():
+        pass    
+
+    def _display_word_lines(self):
+        for i in range(len(self._wordd)):
+            if self._indice==i:
+                print(f"{self._word[i]}", end=" ")
+            else:
+                print("_", end="  ")
